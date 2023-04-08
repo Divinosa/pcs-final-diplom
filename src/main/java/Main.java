151e6b98757a037b80e4bf6c714d1038a5ee1e6f
Danl <Dalnas@mail.ru>
2023-04-08 22:24:07 +0300

    add Server

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
-import java.io.File;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs\\SoftSkills.pdf"));
-        System.out.println(engine.search("уже"));
+        //System.out.println(engine.search("уже"));
+
+
+
 
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
---
+++ src/main/java/Server.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public Server(){
+        try (ServerSocket serverSocket = new ServerSocket(8989);) { // стартуем сервер один(!) раз
+            while (true) { // в цикле(!) принимаем подключения
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    System.out.println("Поиск...");
+                    final String word = in.readLine();
+                    //System.out.println(engine.search(word));
+                    // обработка одного подключения
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
+    }
+}
